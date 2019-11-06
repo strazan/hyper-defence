d99ad322c19c42830c0ddedb6109e74d596006a5
strazan <sigge@labor.cool>
2019-11-06 18:41:34 +0100

    moving enemy. such success

---
+++ src/scripts/enemy.js
@@ -0,0 +1,71 @@
+class Enemy {
+    constructor(x, y, path, ts, sk) {
+        // this.color = [0, 0, 0];
+        this.radius = 0.5; // radius in tiles
+        this.sk = sk
+        this.alive = true;
+        this.path = path;
+
+        this.pos = sk.createVector(x, y);
+        this.vel = sk.createVector(0, 0);
+
+        this.name = 'enemy';
+        this.cash = 1;
+        this.health = 2;
+        this.speed = 1;
+        this.ts = ts;
+
+    }
+
+    draw() {
+        //   console.log(sk)
+        this.sk.fill(150, 150, 150);
+        // sk.fill(this.getColor());
+        this.e = this.sk.ellipse(this.pos.x, this.pos.y, this.radius * this.ts, this.radius * this.ts);
+    }
+
+    steer() {
+        if (this.alive) {
+
+            let t = this.sk.createVector(Math.floor(this.pos.x / this.ts), Math.floor(this.pos.y / this.ts))
+            let tileOn = this.path.filter((p) => {
+                return t.x === p.x && t.y === p.y
+            })[0]
+
+            let goal = this.path[this.path.indexOf(tileOn) + 1]
+
+            if (!goal) {
+                goal = this.path[this.path.indexOf(tileOn)]
+                this.alive = false
+
+            }
+            // console.log(t)
+            if (t.x < goal.x) {
+                this.vel = this.sk.createVector(2, 0)
+            }
+            if (t.x > goal.x) {
+                this.vel = this.sk.createVector(-2, 0)
+            }
+            if (t.y < goal.y) {
+                this.vel = this.sk.createVector(0, 2)
+            }
+            if (t.y > goal.y) {
+                this.vel = this.sk.createVector(0, -2)
+            }
+
+        }
+    }
+
+    move() {
+        if (!this.alive) return
+        this.pos.add(this.vel)
+        this.draw()
+    }
+
+
+
+}
+
+module.exports = {
+    Enemy
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,68 @@
-import './../style.css';
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const context = canvas.getContext('2d');
-const tileWidth = Math.floor(window.innerWidth / 16);
-const tileHeight = Math.floor(window.innerHeight / 16);
-const main = require('./map')
-
-console.log(main.getMap())
-setInterval(() => {
-  makePath(main.getMap())
-}, 27);
+import './../style.css'
+import {
+  Enemy
+} from './enemy'
+import * as p5 from './../scripts/p5.min'
 
 
-function makePath(map) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+const tileWidth = Math.floor(window.innerHeight / 16)
+const tileHeight = Math.floor(window.innerHeight / 16)
+const ts = Math.floor(window.innerHeight / 16)
+const _map = require('./map')
 
+let map = _map.getMap().maze
+let path = _map.getMap().path
+let sketch;
+let spawn = _map.getMap().spawn
+let e;
+
+
+let enemies = []
+let s = (sk) => {
+  sk.setup = () => {
+    sk.stroke(255)
+    sketch = sk;
+    var div = document.getElementById('canvas-holder')
+    let canvas = sk.createCanvas(window.innerHeight, window.innerHeight)
+    canvas.parent(div)
+    drawMap(map, sk)
+    setInterval(() => {
+      enemies.push(new Enemy(ts * spawn.x, spawn.y * ts, path, ts, sk))
+    }, 2000);
+  }
+
+  sk.draw = () => {
+    drawMap(map, sk)
+    enemies.forEach(e => {
+      e.steer()
+      e.move()
+    })
+
+
+  }
+}
+
+function drawMap(map, sk) {
   map.forEach((row, y) => {
     row.forEach((tile, x) => {
       switch (tile) {
         case 0:
-          context.fillStyle = '#fff';
+          sk.fill(255);
           break;
         case 1:
-          context.fillStyle = '#000';
+          sk.fill(0);
           break;
         case 2:
-          context.fillStyle = 'salmon';
+          sk.fill(100);
           break;
       }
-      context.fillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+      sk.rect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
     });
-
   });
 }
+
+function p5draw(sk) {
+
+}
+
+const P5 = new p5(s);
