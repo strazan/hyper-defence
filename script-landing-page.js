9aef8f62c25e88bfd7cf3a0138778263ca8104fb
Emelie <emelie.edman@hyperisland.se>
2019-11-12 12:39:06 +0100

    changed grid layout and boxes img

---
+++ script-landing-page.js
@@ -0,0 +1,11 @@
+const playButton = document.getElementById('play-button');
+const readmoreButton = document.getElementById('read-more-button');
+
+
+playButton.onclick = function() {
+    alert('you clicked me!');
+}
+
+readmoreButton.onclick = function() {
+    alert('you clicked me!');
+}
